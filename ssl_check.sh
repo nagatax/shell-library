8bd5de67b7a1ab1018aa883e27337ec23c591252
nagatax <4foview@gmail.com>
2018-12-20 14:04:01 +0000

    処理のリファクタリング
    処理を見直しました。

--- ssl_check.sh
+++ ssl_check.sh
@@ -4,11 +4,15 @@
 
 ########## 共通処理  ##########
 
+notice() {
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@"
+}
+
 err() {
     echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
 }
 
-function usage() {
+usage() {
     # 引数のチェック
     if [[ -z $1 ]]; then
         err "Usage: certificate file not found"
@@ -16,34 +20,37 @@ function usage() {
     fi
 }
 
+# SSL証明書の情報を取得する
+ssl_infomation() {
+    openssl x509 -noout -text -in ${1}    |
+        sed -n "/${2}/s/.* : \(.*\)/\1/p"
+}
+
+# 秒数へ変換する
+convert_sec() {
+    if [ -p /dev/stdin ]; then
+        date -f "${1}" "+%s"
+    else
+        date -d "${1}" "+%s"
+    fi
+}
+
 ###############################
 
 ######### メイン処理  #########
 
-function main() {
+main() {
+    # 入力値チェック
+    usage "$@"
     
-    local certificate_file_path
-    local cmd_get_certificate_Info
-    local end_date
-    local end_date_sec
-    local now_sec
-    local diff_day
+    local ssl_after_date_sec 
+    ssl_after_date_sec=$( ssl_infomation "${1}" "Not After" | convert_sec - )
 
-    # 証明書ファイルのpathを設定する
-    certificate_file_path="${1}"
-    
-    usage "${certificate_file_path}"
-    
-    # 証明書の期限を取得する
-    cmd_get_certificate_Info="openssl x509 -noout -text -in ${certificate_file_path}"
-    end_date=$(date -d "$( eval ${cmd_get_certificate_Info} | sed -n '/Not After/s/.* : \(.*\)/\1/p')" '+%Y/%m/%d 00:00:00')
-    end_date_sec=$(date -d "${end_date}" "+%s")
-    
-    # 処理日の秒数を取得する
-    now_sec=$(date -d "00:00:00" '+%s')
-    
-    # 証明書の期限日と処理日の差分を取得する
-    diff_day=$(( (${end_date_sec} - ${now_sec}) / (24 * 60 * 60) ))
+    local now_sec
+    now_sec=$( convert_sec  )
+
+    local diff_day
+    diff_day=$(( (${ssl_after_date_sec} - ${now_sec}) / (24 * 60 * 60) ))
     
     if [[ ${diff_day} -lt 0 ]]; then
         # 有効期限切れ
@@ -57,3 +64,4 @@ function main() {
 ###############################
 
 main "$@"
+
