b957d9bce9e6467fc51dbb2d14ca663ac5329d8a
nagatax <4foview@gmail.com>
2018-12-21 14:17:01 +0900

    プログラムのリファクタリング
    プログラムの構造を変更しました。

--- git_status_change.sh
+++ git_status_change.sh
@@ -19,17 +19,17 @@
 ##########################################################
 function main() {
 
-  git filter-branch -f --env-filter '
-  echo $GIT_AUTHOR_NAME
-  if [ ${GIT_AUTHOR_NAME} = ${1} ]; then
-    local name=${2}
-    local email=${3}
-    
-    export GIT_AUTHOR_NAME=${name}
-    export GIT_AUTHOR_EMAIL=${email}
-    export GIT_COMMITTER_NAME=${name}
-    export GIT_COMMITTER_EMAIL=${email}
-  fi'
+  local target="${1}"
+  local name="${2}"
+  local email="${3}"
+
+  git filter-branch -f --env-filter "
+  if [ \"\${GIT_COMMITTER_NAME}\" = '${target}' ]; then
+    export GIT_AUTHOR_NAME='${name}'
+    export GIT_AUTHOR_EMAIL='${email}'
+    export GIT_COMMITTER_NAME='${name}'
+    export GIT_COMMITTER_EMAIL='${email}'
+  fi"
 
   return #?
 }
